8c2416c2e595baaa00b5f70a566f6e114286accc
Sped32DJ <dannycool10236@gmail.com>
2023-02-27 12:49:25 -0800

    resolution QOL added

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-#;neofetch
+#neofetch
 fish_vi_key_bindings
 set fish_greeting
 
@@ -21,7 +21,7 @@ alias google="ping google.com -c3"
 alias weather="curl 'wttr.in/{SanBernardino,Riverside}?format=3'" #Find a way for this to be on Waybar
 alias temp="curl 'wttr.in/?format=%t'"
 
-alias netrestart="nmcli networking off && nmcli networking on && nmcli reload && echo BetterWork"
+alias netrestart="nmcli networking off && nmcli networking on && echo BetterWork"
 alias netschool="nmcli con up UCR-SECURE"
 
 alias mirror="doas reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
@@ -32,5 +32,13 @@ alias pipes="pipes -f 120 -R"
 alias doas="doas --"
 alias storage="ncdu"
 
+# wlr-randr
+# Probably add a "Low power" alias next, removes blur,animations, Goes 60Hz, etc
+alias 60Hz="wlr-randr --output eDP-2 --mode 2560x1600@60Hz  --scale 1"
+alias 120Hz="wlr-randr --output eDP-2 --mode 2560x1600@120Hz --scale 1"
+
+# Hyprland stuff
+export SDL_VIDEODRIVER=wayland
+export MOZ_ENABLE_WAYLAND=1
 
 starship init fish | source
